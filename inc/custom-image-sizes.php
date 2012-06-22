9f7de9846ded6cd1f2bc8d351e2492f4b640c057
Dane Odekirk <dizzzy@uw.edu>
2012-06-22 15:01:06 -0700

    preview button for showcase widget. hiding thimble from media size selector

--- inc/custom-image-sizes.php
+++ inc/custom-image-sizes.php
@@ -15,7 +15,7 @@ function uw_get_additional_image_sizes() {
 	global $_wp_additional_image_sizes;
 	if ( isset($_wp_additional_image_sizes) && count($_wp_additional_image_sizes) ) {
 		$sizes = apply_filters( 'intermediate_image_sizes', $_wp_additional_image_sizes );
-		$sizes = apply_filters( 'uw_get_additional_image_sizes', $_wp_additional_image_sizes );
+		//$sizes = apply_filters( 'uw_get_additional_image_sizes', $_wp_additional_image_sizes );
 	}
 
 	return $sizes;
@@ -36,8 +36,11 @@ function uw_additional_image_size_input_fields( $fields, $post ) {
 		$css_id = "image-size-{$size}-{$post->ID}";
 		$label = apply_filters( 'uw_image_size_name', $size );
 
-    // remove tile sizes from selection
+    // remove Tile sizes from selection
     if ( is_numeric(strpos($size, 'Tile') )) continue; 
+
+    // remove Thimble sizes from selection
+    if ( is_numeric(strpos($size, 'Thimble') )) continue; 
 
 		$html  = "<div class='image-size-item'>\n";
 		$html .= "\t<input type='radio' " . disabled( $enabled, false, false ) . "name='attachments[{$post->ID}][image-size]' id='{$css_id}' value='{$size}' />\n";
@@ -50,6 +53,7 @@ function uw_additional_image_size_input_fields( $fields, $post ) {
 	}
 
 	$items = join( "\n", $items );
+	$fields['image-size']['html'] .= "<hr/>";
 	$fields['image-size']['html'] = "{$fields['image-size']['html']}\n{$items}";
 
 	return $fields;
--- inc/custom-widgets.php
+++ inc/custom-widgets.php
@@ -711,7 +711,10 @@ class UW_Showcase_Widget extends WP_Widget {
 
     if ( ! empty( $title ) ) echo $before_title . $title . $after_title;
 
-    echo apply_filters('the_content', $instance['content']); 
+    switch_to_blog(1);
+    $post = get_post($instance['id']);
+    echo apply_filters('the_content', $post->post_content);
+    restore_current_blog();
 
     if (is_super_admin()) 
       echo '<a class="pull-right" target="_blank" href="' . $instance['edit'] . '">Edit</a>';
@@ -728,13 +731,14 @@ class UW_Showcase_Widget extends WP_Widget {
 
 		$instance['id'] = $new_instance['id'];
 		$instance['title']   = strip_tags( $post->post_title );
-		$instance['content'] = $post->post_content;
+		//$instance['content'] = $post->post_content;
     $instance['edit'] = $edit;
 
 		return $instance;
 	}
 
 	public function form( $instance ) {
+    wp_enqueue_script('jquery-ui-dialog');
     $cat = get_term_by('slug','showcase-widget', 'category');
     $args = array(
       'numberposts' => -1,
@@ -748,15 +752,75 @@ class UW_Showcase_Widget extends WP_Widget {
 
 		<input class="widefat hidden" disabled="disabled" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 
-    <p>
 		<label for="<?php echo $this->get_field_id('id'); ?>"><?php _e( 'Choose content:' ); ?></label>
-			<select name="<?php echo $this->get_field_name('id'); ?>" id="<?php echo $this->get_field_id('id'); ?>" class="widefat">
+      <a class="alignright preview-showcase" id="preview-widget-<?php echo $this->get_field_id('id'); ?>" href="#preview">Preview</a>
+			<select name="<?php echo $this->get_field_name('id'); ?>" id="<?php echo $this->get_field_id('id'); ?>" class="widefat showcase-select">
 
       <?php foreach($posts as $post) : ?>
         <option value="<?php echo $post->ID; ?>"<?php selected( $instance['id'], $post->ID); ?>><?php _e($post->post_title); ?></option>
       <?php endforeach; ?>
 
 			</select>
+
+      <style type="text/css">
+        .preview-showcase-widget h2 {
+          font-family: 'Open Sans', sans-serif; font-weight: 400; letter-spacing: -.05em; color: #39275B;
+        }
+        .preview-showcase-widget a {
+          color: #3089C2;
+          text-decoration:none;
+        }
+        .preview-showcase-widget li {
+          margin-left: 0;
+          margin-bottom: 5px;
+          background: url('/cms/wp-content/themes/uw/img/bullet-gold.png') no-repeat left 7px transparent;
+          list-style: none;
+          padding-left: 12px;
+         }
+        .preview-showcase-widget p {
+          font-size: 14px; line-height: 21px; 
+          color: #333;
+          font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        }
+      </style>
+
+      <?php foreach($posts as $post) : ?>
+
+        <div class="hidden preview-showcase-widget post-<?php echo $post->ID; ?>">
+          <h2><span><?php echo$post->post_title; ?></span></h2>
+          <?php echo apply_filters('the_content', $post->post_content); ?>
+        </div>
+
+      <?php endforeach; ?>
+
+      <script type="text/javascript">
+        jQuery(document).ready(function($) {
+          
+                // set up a jquery boolean so this script only runs once on the page and not once per showcase widget
+                if ( $.fn.showcase_widget_preview_enabled ) 
+                  return;
+
+                var length = $('select.showcase-select').first().find('option').length
+                  console.log(length);
+                $('.preview-showcase-widget').slice(0,length)
+                  .dialog({
+                    autoOpen: false,
+                    width:265,
+                    modal:true
+                 })
+
+               $('body').on('click', 'a.preview-showcase', function() {
+                 var $this = $(this)
+                   , id    = $this.siblings('select').val()
+
+                 $('.post-'+id).dialog('open');
+
+               });
+
+                $.fn.showcase_widget_preview_enabled = true;
+
+        });
+      </script>
 		</p>
 		<?php 
 	}
