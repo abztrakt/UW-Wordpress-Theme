abc2e73a3d06d8b4d03e4acc1c96e0e83bd81f0b
Dane Odekirk <dizzzy@uw.edu>
2012-07-16 09:53:11 -0700

    make the categories widget dropdown use jquery

--- inc/custom-widgets.php
+++ inc/custom-widgets.php
@@ -19,6 +19,7 @@ function uw_register_widgets() {
 	register_widget('UW_Widget_MailChimp');
   register_widget('UW_Widget_CommunityPhotos');
   //register_widget('UW_Widget_Showcase_Links');
+  register_widget('UW_Widget_Categories');
   register_widget('UW_Widget_Twitter');
   register_widget('UW_KEXP_KUOW_Widget');
   register_widget('UW_Showcase_Widget');
@@ -206,6 +207,68 @@ function uw_patch_link_widget( $args ) {
 }
 
 add_filter('widget_links_args', 'uw_patch_link_widget');
+
+/**
+ *
+ *
+ *
+ * Instead of overriding the dropdown widget we will patch it by extending its class
+ *
+ *
+ ***********************************************************************************/
+
+class UW_Widget_Categories extends WP_Widget_Categories {
+
+  	function widget( $args, $instance ) {
+      extract( $args );
+
+      $title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Categories' ) : $instance['title'], $instance, $this->id_base);
+      $c = ! empty( $instance['count'] ) ? '1' : '0';
+      $h = ! empty( $instance['hierarchical'] ) ? '1' : '0';
+      $d = ! empty( $instance['dropdown'] ) ? '1' : '0';
+
+      echo $before_widget;
+      if ( $title )
+        echo $before_title . $title . $after_title;
+
+      $cat_args = array('orderby' => 'name', 'show_count' => $c, 'hierarchical' => $h);
+
+      if ( $d ) {
+        $cat_args['show_option_none'] = __('Select Category');
+        wp_dropdown_categories(apply_filters('widget_categories_dropdown_args', $cat_args));
+  ?>
+
+  <script type='text/javascript'>
+  /* <![CDATA[ */
+        jQuery(document).ready(function($) {
+          $("#cat").change(function() {
+            var val = $(this).val();
+            if ( val > 0 ) {
+              location.href = "<?php echo home_url(); ?>/?cat="+val;
+            }
+          })
+        });
+  /* ]]> */
+  </script>
+
+  <?php
+      } else {
+  ?>
+      <ul>
+  <?php
+      $cat_args['title_li'] = '';
+      wp_list_categories(apply_filters('widget_categories_args', $cat_args));
+  ?>
+      </ul>
+  <?php
+      }
+
+		echo $after_widget;
+	}
+
+
+}
+
 
 
 
