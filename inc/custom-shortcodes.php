a85a4acef78697056287942e8e1ab54416563e62
Dane Odekirk <dizzzy@uw.edu>
2012-08-28 11:13:41 -0700

    adding rss shortcode

--- inc/custom-shortcodes.php
+++ inc/custom-shortcodes.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * The Caption shortcode filter - original function in wp-includes/media.php
- *
  */
 
 add_filter('img_caption_shortcode', 'add_photo_credit_to_img_caption_shortcode',10,3);
@@ -34,4 +33,65 @@ if ( !function_exists('add_photo_credit_to_img_caption_shortcode')) :
 
 endif;
 
-?>
+/**
+ * The RSS Shortcode
+ */
+
+if ( ! function_exists('uw_feed_shortcode') ):
+  function uw_feed_shortcode( $atts ) 
+  {
+    extract( shortcode_atts( array(
+        'url'    => null,
+        'number' => 5,
+        'title'  => null,
+        'span'   => 4
+      ), $atts ) );
+    
+    if ( $url == null )
+      return '';
+
+    $content = '';
+
+    $feed = fetch_feed($url);
+
+
+    if (!is_wp_error( $feed ) ) 
+    { 
+      $url = $feed->get_permalink();
+      $feed_items = $feed->get_items(0, $number); 
+      $feed_items = $feed->get_items(0, $number); 
+
+      $title = ($title == null ) ? $feed->get_title() : $title;
+
+      $content = "<div class=\"row pull-left\">";
+      $content .= "<div class=\"span$span row\">";
+      $content .= "<div class=\"feed-in-body\"><h3>$title</h3></div>";
+      $content .= "<ul>";
+
+      foreach ($feed_items as $index=>$item) 
+      {
+          $title = $item->get_title();
+          $link  = $item->get_link();
+          $attr  = esc_attr(strip_tags($title));
+          $content .= "<li><a href=\"$link\" title=\"$attr\">$title</a></li>";
+      }
+
+      $content .= '</ul>';
+      $content .= "<a class=\"feed-more\" href=\"$url\">More</a>";
+      $content .= "</div></div>";
+    }
+    return $content;
+  }
+endif;
+add_shortcode( 'rss', 'uw_feed_shortcode' );
+
+function bartag_func($atts) {
+     extract(shortcode_atts(array(
+	      'foo' => 'no foo',
+	      'baz' => 'default baz',
+     ), $atts));
+     return "foo = {$foo}";
+}
+add_shortcode('bartag', 'bartag_func');
+
+
