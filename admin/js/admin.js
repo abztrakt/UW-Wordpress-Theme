e06e52e53e0a052d64a2c437f74a42d8f71795df
Dane Odekirk <dizzzy@uw.edu>
2013-01-17 11:35:14 -0800

    media alt text will use the title if left blank

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,3 +1,17 @@
 jQuery(document).ready(function($){
   $('input[name=noconfirmation]').closest('tr').hide().end().attr('checked', true);
+
+
+  // For media, if the alt is left blank use the title as the default value
+  $('body').on('click', '#save', function() {
+    var $alt = $('tr.image_alt')
+      , $title = $('tr.post_title')
+
+    if ( $alt.length == 1 && $alt.find('input').val() == '' ) 
+    {
+      $alt.find('input').val($title.find('input').val())
+    }
+      
+  });
+
 })
