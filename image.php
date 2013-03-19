b1e63d49704e62c7a54cdad002f536a649aedda5
Dane Odekirk <dizzzy@uw.edu>
2013-03-19 15:30:15 -0700

    image caption shows up in the image view

--- image.php
+++ image.php
@@ -18,7 +18,7 @@
 				<div class="entry-content">
 					<?php echo wp_get_attachment_image($post->ID, 'full'); ?>
 					<h1 class="entry-title"><?php the_title(); ?></h1>
-					<?php the_content(); ?>
+					<?php the_excerpt(); ?>
           <div><a href="<?php echo wp_get_attachment_url(get_the_ID());?>" title="<?php the_title(); ?>" target="_blank" download="<?php the_title() ?>">Download</a></div>
 				</div><!-- .entry-content -->
 				<footer class="entry-meta">
