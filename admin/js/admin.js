3f13e8bf316b00823d964bb5ea58e22551972624
Dane Odekirk <dizzzy@uw.edu>
2013-04-16 16:31:44 -0700

    added live word count for post excerpts

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($){
 
+  var $excerpt = $('#excerpt')
+
   // Force checks the no-confirmation necessary when registering users
   $('input[name=noconfirmation]').closest('tr').hide().end().attr('checked', true);
 
@@ -15,5 +17,28 @@ jQuery(document).ready(function($){
     }
       
   });
+
+
+  if ( !$excerpt.length )
+    return;
+
+  $excerpt
+      .bind('keyup', function() {
+        var $this = $(this)
+          , $count = $('#excerpt-word-count')
+          , wordc  = $this.val().split(' ').length - 1
+          , limit  = 55
+          , str    = wordc == limit-1 || wordc == limit+1 ? ' word left)' : ' words left)'
+
+        if ( wordc > limit ) 
+          str = str.replace('left', 'over')
+        
+        $count.html( '(' + Math.abs(limit - wordc) + str )
+
+      })
+      .closest('#postexcerpt').find('h3')
+        .append(' <small id="excerpt-word-count"></small>')
+        .end().end()
+        .trigger('keyup')
 
 })
