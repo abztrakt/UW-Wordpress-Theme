1da077d2b16bf3a613cc9d06909df93fd2e04fe0
Dane Odekirk <dizzzy@uw.edu>
2013-07-05 13:12:13 -0700

    adding custom fields to coauthors plus guest author fields

--- admin/custom-user-info-fields.php
+++ admin/custom-user-info-fields.php
@@ -17,4 +17,19 @@ if (!function_exists('additional_contact_fields')) :
 
 endif;
 
-add_filter('user_contactmethods','additional_contact_fields',10,1);
+function uw_filter_guest_author_fields( $fields_to_return, $groups ) {
+ 
+	if ( in_array( 'all', $groups ) || in_array( 'name', $groups ) ) {
+		$fields_to_return[] = array(
+					'key'      => 'affiliation',
+					'label'    => 'Affiliation',
+					'group'    => 'name',
+				);
+	} 
+ 
+	return $fields_to_return;
+}
+
+
+add_filter( 'user_contactmethods','additional_contact_fields',10,1);
+add_filter( 'coauthors_guest_author_fields', 'uw_filter_guest_author_fields', 10, 2 );
